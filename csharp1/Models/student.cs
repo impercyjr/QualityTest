f68b2971b6d0226745200e0f69c75cef9b219933
NGuyen Trung Sy <syntth1808031@fpt.edu.vn>
2019-12-07 21:29:35 +0700

    QualityTest

---
+++ csharp1/Models/student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp1.models
+{
+    class Student
+    {
+        public string RollNumber { get; set; }
+        public string FullName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public DateTime CreateAt { get; set; }
+        public Status status { get; set; }
+        
+
+    }
+
+        public bool IsNewStudent()
+    {
+        return (DateTime.Now - this.CreateAt).TotalDay < 60);
+    }
+    public bool IsDeactive()
+    {
+        if (this.status == 0)
+        {
+            return true;
+        }
+        else return false;
+    }
+    public enum Status
+    {
+        Active = 1
+        Deactive = 0
+
+    }
+
+}
